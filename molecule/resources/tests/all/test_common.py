6fea717fa83e68281d82c5ce0380fda4ea0e97cb
Andrea Tosatto <andrea.tosy@gmail.com>
2019-02-25 15:41:06 +0100

    Add an option to configure a cronjob to cleanup old Metronome data (#6)

--- molecule/resources/tests/all/test_common.py
+++ molecule/resources/tests/all/test_common.py
@@ -14,11 +14,11 @@ rhel_os = ['redhat', 'centos']
 
 @pytest.fixture()
 def AnsibleVars(host):
-    varsFiles = ["../../vars/main.yml"]
+    varsFiles = ['../../vars/main.yml']
     if host.system_info.distribution.lower() in debian_os:
-        varsFiles.append("../../vars/Debian.yml")
+        varsFiles.append('../../vars/Debian.yml')
     if host.system_info.distribution.lower() in rhel_os:
-        varsFiles.append("../../vars/RedHat.yml")
+        varsFiles.append('../../vars/RedHat.yml')
 
     ansibleVars = {}
     for f in varsFiles:
@@ -40,7 +40,7 @@ def test_package(host):
 def test_service(host):
     # Using Ansible to mitigate some issues with the service test on debian-8
     s = host.ansible('service', 'name=metronome state=started enabled=yes')
-    assert s["changed"] is False
+    assert s['changed'] is False
 
 
 def test_localjs_config(host, AnsibleVars):
@@ -52,7 +52,8 @@ def test_localjs_config(host, AnsibleVars):
 
 
 def test_service_config(host):
-    smgr = host.ansible("setup")["ansible_facts"]["ansible_service_mgr"]
+    f = None
+    smgr = host.ansible('setup')['ansible_facts']['ansible_service_mgr']
     if smgr == 'systemd':
         f = host.file('/etc/systemd/system/metronome.service.d/override.conf')
         assert 'LimitCORE=infinity' in f.content
@@ -62,3 +63,15 @@ def test_service_config(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+    assert oct(f.mode) == '0644'
+
+
+def test_cleanup_cronjob(host):
+    f = None
+    if host.system_info.distribution.lower() in debian_os:
+        f = host.file('/var/spool/cron/crontabs/root')
+    if host.system_info.distribution.lower() in rhel_os:
+        f = host.file('/var/spool/cron/root')
+
+    assert f.exists
+    assert '15 2 * * * find /var/lib/metronome/ -mtime +5 -delete' in f.content
